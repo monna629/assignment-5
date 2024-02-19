f9800e40e0919af3ffbf3b70f748fb4dd11d5184
monna629 <aminulhaquemonna@gmail.com>
2024-02-19 15:06:17 +0600

    updated

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const allSeats = document.getElementsByClassName("seats");
+let count = 0;
+for (const seat of allSeats) {
+  seat.addEventListener("click", function () {
+    count = count + 1;
+    setInnerText("total-select", count);
+    seat.style.backgroundColor = "#1DD100";
+    document.getElementById("remaining-seat").innerText--;
+    const appendLi = document.getElementById("append-here");
+  });
+}
 
+function setInnerText(id, value) {
+  document.getElementById(id).innerText = value;
+}
